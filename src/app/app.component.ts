653d5b7e3c5e99489d1b8e02e18fa4e07ee8be76
Patryk Korczak <patkor9@gmail.com>
2024-05-16 19:46:25 +0200

    Implement starting portfolio components such as: main, nav, footer along with simple routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { NavComponent } from './component/nav/nav.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { MainComponent } from './component/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'portfolio';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProjectListComponent } from './project/project-list/project-list.component';
+import { HomeComponent } from './home/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'projects', component: ProjectListComponent},
+    { path: '', component: HomeComponent}
+];
